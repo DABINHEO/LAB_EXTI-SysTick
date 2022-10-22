781bf3fc746ae12a217f54dba7acb0239074f8ef
DABINHEO <113574284+DABINHEO@users.noreply.github.com>
2022-10-22 15:55:37 +0900

    Update LAB_EXTI_SysTick.c

--- LAB_EXTI_SysTick.c
+++ LAB_EXTI_SysTick.c
@@ -11,21 +11,29 @@
 #include "ecRCC.h"
 #include "ecGPIO.h"
 #include "ecSysTick.h"
+#include "ecEXTI.h"
 
 #define LED_PIN 	5
 #define BUTTON_PIN 13
 
+int SEVENSEGMENT_NUM = 0;
 void setup(void);
 
 int main(void) {
 	// System CLOCK, Systick, sevensegment Initialiization ----------------------------------------
 	setup();
-	int num = 0;
-
+	
 	while (1){
-		sevensegment_decode(num%10);
-		num++;
+		sevensegment_decode(SEVENSEGMENT_NUM%10);
+		SEVENSEGMENT_NUM++;
 		delay_ms(1000);
+	}
+}
+
+void EXTI15_10_IRQHandler(void) {
+	if (is_pending_EXTI(BUTTON_PIN)) {
+		SEVENSEGMENT_NUM = 0;
+		clear_pending_EXTI(BUTTON_PIN); 		// cleared by writing '1'
 	}
 }
 
@@ -33,7 +41,12 @@ int main(void) {
 void setup(void)
 {
 	RCC_PLL_init();                         // System Clock = 84MHz
+	// Initialize GPIO for sevensegment
 	sevensegment_init();
+	// Initialize Systick
 	SysTick_init();
+	// Initialize GPIOC_13 for Input Button
+	GPIO_init(GPIOC, BUTTON_PIN, INPUT);  // calls RCC_GPIOC_enable()
+	GPIO_pupd(GPIOC, BUTTON_PIN, EC_PU);
+	EXTI_init(GPIOC, BUTTON_PIN, FALL, 2);
 }
-
